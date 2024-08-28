4167925cdf7dc03394eca2f743495dc2c9648caa
Ethan Hall <ehall260@cedarville.edu>
2024-08-28 15:01:36 -0400

    Initial Commit

---
+++ Restuarant Reviews/Restuarant_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restuarant_ReviewsApp.swift
+//  Restuarant Reviews
+//
+//  Created by Ethan Hall on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Restuarant_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
